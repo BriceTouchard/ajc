4fcda2edcc47423239c771873532f385244acf9b
Brice Touchard <touchardbrice@gmail.com>
2023-08-01 14:27:18 +0200

    piechart avec options 3d et bkgcolor

--- piechart/src/main.c
+++ piechart/src/main.c
@@ -29,52 +29,68 @@ int compter(char *n){
     return nbrcat;
 }
 
-int main (int argc, char *argv[])
-{
-    int nbrcat=compter(argv[1]);
-
-    // Récupère les pourcentages, prct
-    int prct[nbrcat];
-    char * token = strtok(argv[1], ",");     // Premier token
+void pourcentages(char* argvstr,int *prct){
+    char * token = strtok(argvstr, ","); // Premier token
     int j=0;
     while( token != NULL ) {
        sscanf(token, "%d", &prct[j]);
        token = strtok(NULL, ",");
        j++;
     }
+}
 
-    // Initialisation de noms
-    char **noms = malloc(sizeof(char*) * nbrcat);
-    for(int i = 0; i < nbrcat; i++) {
-        noms[i] = malloc(sizeof(char) * 20);
-    }
-
-    // On récupère les noms
-    char * tok2 = strtok(argv[2], ",");     // Premier token
+void nom(char* argvstr,char** noms){
+    char * tok2 = strtok(argvstr, ","); // Premier token
     int k=0;
     while( tok2 != NULL ) {
        strcpy(noms[k],tok2);
        tok2 = strtok(NULL, ",");
        k++;
     }
+}
+
+int main (int argc, char *argv[])
+{
+    int nbrcat=compter(argv[1]);
+    int prct[nbrcat];
+    int rvb[3] = {255,255,255}; // Fond blanc par défaut
+    char **noms = malloc(sizeof(char*) * nbrcat);
+    for(int i = 0; i < nbrcat; i++) {
+        noms[i] = malloc(sizeof(char) * 20);
+    }
+    pourcentages(argv[1],prct); // récupère les pourcentages et les place dans prct
+    nom(argv[2],noms);          // récupère les noms et les places dans noms
+    char *titre = argv[3];      // récupère le titre
+
+    // todo : for pour parcourir les args et switch case pour les différentes options
+
+    int persp=0;
+    if(argv[4] != NULL && (strcmp(argv[4], "3d") == 0)){
+        persp=1; // on opte pour la perspective 3d
+    }
+
+    if(argv[5] != NULL){
+        printf("couleur = %s\n",argv[5]);
+        pourcentages(argv[5],rvb);
+    }
+
+    int trait = 10;         // taille des traits de légende
+    int tdist = 50;         // Distance texte-camembert
+    int diax = 400;         // diamètre horizontal du camembert
+    int diay;               // diamètre vertical du camembert
+    if (persp==1)
+        diay=diax-100;
+    else
+        diay=diax;
+    int imgsize = 550;      // taille image
+    int epais = 50;         // épaisseur (perspective 3D)
+    int black, white, jaune, rose, orange, fuschia, vert, bleu, marron, gris; // Noms des couleurs (8)
 
     gdImagePtr im;
     FILE *pngout;
-    // exemple d'entrée : pie -p 40,50,10 -l USA,CANADA,FRANCE Alcooliques.png
-
-    char *noms1[20] = {"USA","UK","Canada","Other","Australia","France","Espagne","Allemagne","Italie","Belgique"}; // 10
-    char *titre = argv[3];
-
-    // Dimensions, couleurs, mise en forme
-    int trait = 10;         // taille des traits de légende
-    int tdist = 50;         // Distance texte-camembert
-    int dia = 400;          // diamètre du camembert
-    int imgsize = 550;      // taille image
-    int black, white, jaune, rose, orange, fuschia, vert, bleu, marron, gris; // Noms des couleurs (8)
-
     im = gdImageCreate(imgsize, imgsize);  //création de l'image
 
-    white =   gdImageColorAllocate(im, 255, 255, 255); // background
+    gdImageColorAllocate(im, rvb[0], rvb[1], rvb[2]); // background
     black =   gdImageColorAllocate(im, 0, 0, 0);
 
     jaune =   gdImageColorAllocate(im, 245, 222, 179);
@@ -96,36 +112,88 @@ int main (int argc, char *argv[])
     fonts[4] = gdFontGetGiant ();
 
     // Remplissage
-    int angle = -90;                 // Départ des portions de 12h, pas 3h (en °)
+    int angle = -90;                // Départ des portions de 12h, pas 3h (en °)
     int angle2 = prct[0]*3.6/2-90;  // Départ des traits de légende (en °)
-    int x1,y1,x2,y2,x3,y3;           // Coordonnées des point finaux des lignes
+    int x1,y1,x2,y2,x3,y3;          // Coordonnées des point finaux des lignes
 
-    /* ce sont des coordonnées absolues, le point de départ est au centre,
-     * le point final est le centre auquel on ajoute le cosinus en abcisse
-     * et le sinus en ordonnée (on projette).*/
+    // On dessine les arc du dessous si on est en 3D
+    if (persp==1) {
+        for (int i = 0; i < nbrcat; i++){
+            // On trace les traits de légendes
+            x1 = imgsize/2 + (diax/2+trait) * cos(angle2/180.*3.14);
+            y1 = imgsize/2 + epais + (diay/2+trait) * sin(angle2/180.*3.14);
+            gdImageLine(im, imgsize/2, imgsize/2+epais, x1, y1, black);
 
+            // arc du dessous
+            gdImageFilledArc(im,imgsize/2,imgsize/2+epais,diax,diay,angle,angle+prct[i]*3.6,colors[i],0);
+            angle+=prct[i]*3.6;
+            angle2=angle+prct[i+1]*3.6/2;
+        }
+    }
+
+    // On stocke les coordonnées des points finaux des lignes délimitant les secions
+    int x,y;
+    int xcoord[nbrcat];
+    int ycoord[nbrcat];
+    for (int i = 0; i < nbrcat; i++) {
+        x = imgsize/2 + diax/2 * cos(angle/180.*3.14);
+        y = imgsize/2 + diay/2 * sin(angle/180.*3.14);
+        xcoord[i]=x;
+        ycoord[i]=y;
+        angle+=prct[i]*3.6;
+    }
+
+    // On dessine le(s) rectangle(s)
+    if (persp==1) {
+        angle = -90;
+        for(int i = 0; i < nbrcat ;i++){ // rectangles inférieurs
+            if (ycoord[i] > imgsize/2 && xcoord[i] > imgsize/2) { //quart inf droit
+                gdImageFilledRectangle(im,xcoord[i],ycoord[i],xcoord[i]-epais,ycoord[i]+epais,colors[i]);
+            }
+            else if (ycoord[i] > imgsize/2 && xcoord[i] < imgsize/2){ //quart inf gauche
+                gdImageFilledRectangle(im,xcoord[i],ycoord[i],xcoord[i]+epais,ycoord[i]+epais,colors[i-1]);
+            }
+            else if(ycoord[i] < imgsize/2 && ycoord[i-1] > imgsize/2){ //cotés
+                gdImageFilledRectangle(im,(imgsize-diax)/2,imgsize/2,(imgsize-diax)/2+epais,imgsize/2+epais,colors[i-1]);
+            }
+            angle+=prct[i]*3.6;
+        }
+        for(int i = 0; i < nbrcat ;i++){ // rectangles latéraux
+            if(ycoord[i]-imgsize/2 > 0 && ycoord[i+1]-imgsize/2 < 0){
+                gdImageFilledRectangle(im,(imgsize-diax)/2,imgsize/2,(imgsize-diax)/2+epais,imgsize/2+epais,colors[i]); // côté gauche
+            }
+            else if(ycoord[i]-imgsize/2 < 0 && ycoord[i+1]-imgsize/2 > 0) {
+                gdImageFilledRectangle(im,(imgsize+diax)/2,imgsize/2,(imgsize+diax)/2-epais,imgsize/2+epais,colors[i]); // côté droit
+            }
+        }
+    }
+
+    // On dessine tout le reste (couche du dessus)
+    angle2 = prct[0]*3.6/2-90;
     for (int i = 0; i < nbrcat; i++) {
         // Coordonnées traits de légendes
-        x1 = imgsize/2 + (dia/2+trait) * cos(angle2/180.*3.14);
-        y1 = imgsize/2 + (dia/2+trait) * sin(angle2/180.*3.14);
-        // Coordonnées des lignes séparatrices
-        x2 = imgsize/2 + dia/2 * cos(angle/180.*3.14);
-        y2 = imgsize/2 + dia/2 * sin(angle/180.*3.14);
+        x1 = imgsize/2 + (diax/2+trait) * cos(angle2/180.*3.14);
+        y1 = imgsize/2 + (diay/2+trait) * sin(angle2/180.*3.14);
         // Coordonnées des textes (noms)
-        x3 = imgsize/2 + (dia/2+tdist) * cos(angle2/180.*3.14);
-        y3 = imgsize/2 + (dia/2+tdist) * sin(angle2/180.*3.14);
+        x3 = imgsize/2 + (diax/2+tdist) * cos(angle2/180.*3.14);
+        y3 = imgsize/2 + (diay/2+tdist) * sin(angle2/180.*3.14);
 
-        gdImageLine(im, imgsize/2, imgsize/2, x1, y1, black);
-        gdImageFilledArc(im,imgsize/2,imgsize/2,dia,dia,angle,angle+prct[i]*3.6,colors[i],0);
-        gdImageLine(im, imgsize/2, imgsize/2, x2, y2, black);
+        if (persp==1 && y1 < imgsize/2){
+            gdImageLine(im, imgsize/2, imgsize/2, x1, y1, black); // trait de légende
+        }
+        else if (persp==0){
+            gdImageLine(im, imgsize/2, imgsize/2, x1, y1, black);
+        }
+        gdImageFilledArc(im,imgsize/2,imgsize/2,diax,diay,angle,angle+prct[i]*3.6,colors[i],0);
+        gdImageLine(im, imgsize/2, imgsize/2, xcoord[i], ycoord[i], black); // lignes délimitantes
         gdImageString (im, fonts[4], x3, y3, noms[i], black);
         angle+=prct[i]*3.6;
         angle2=angle+prct[i+1]*3.6/2;
     }
-    gdImageLine(im, imgsize/2, imgsize/2, imgsize/2, imgsize/2-(dia/2+trait), black); //Ligne de départ (12h)
+    gdImageLine(im, imgsize/2, imgsize/2, imgsize/2, imgsize/2-(diay/2+trait), black); //Ligne de départ (12h)
 
     // Contour
-    gdImageEllipse(im,imgsize/2,imgsize/2,dia,dia,black); //cercle noir
+    gdImageEllipse(im,imgsize/2,imgsize/2,diax,diay,black); //cercle noir
 
     strcat(titre, ".png");
     pngout = fopen(titre, "wb");
